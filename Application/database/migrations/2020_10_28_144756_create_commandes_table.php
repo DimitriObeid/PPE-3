cab709c72d53cb7b5b41a9caeeab24e4350c35f5
Rockvald <mathieuprot@orange.fr>
2020-10-28 15:53:41 +0100

    Ajout de l'application

---
+++ Application/database/migrations/2020_10_28_144756_create_commandes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommandesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commandes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_fournitures');
+            $table->integer('id_personnel');
+            $table->timestamps();
+            $table->string('etat', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commandes');
+    }
+}
---
+++ Application/database/migrations/2020_10_28_144851_create_personnel_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonnelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personnel', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom', 255);
+            $table->string('prenom', 255);
+            $table->string('mail', 255);
+            $table->string('pass', 255);
+            $table->string('service', 255);
+            $table->string('categorie', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personnel');
+    }
+}
