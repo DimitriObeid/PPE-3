9d8c312a63efc1edaace3ba503d4aa21b3e9a5a7
Mamadou BARRY <62642252+mmdev12@users.noreply.github.com>
2020-12-17 23:31:53 +0100

    Update web.php

--- Application/routes/web.php
+++ Application/routes/web.php
@@ -25,3 +25,74 @@ Route::get('/', function () {
 });
 
 Route::get('/connexion', [PersonnelController::class, 'connexion']);
+
+// Mamadou 
+
+
+/* FrontEnd  */
+Route::get('/','IndexController@index');
+Route::get('/list-products','IndexController@shop');
+Route::get('/cat/{id}','IndexController@listByCat')->name('cats');
+Route::get('/product-detail/{id}','IndexController@detialpro');
+////// les  Attribut ////////////
+Route::get('/get-product-attr','IndexController@getAttrs');
+
+///// Commandes /////////
+Route::post('/addToCart','CartController@addToCart')->name('addToCart');
+Route::get('/viewcart','CartController@index');
+Route::get('/cart/deleteItem/{id}','CartController@deleteItem');
+Route::get('/cart/update-quantity/{id}/{quantity}','CartController@updateQuantity');
+/////////////////////////
+/// les codes Coupons
+//Route::post('/apply-coupon','CouponController@applycoupon');
+/// les Utilisateurs simples /////
+Route::get('/login_page','UsersController@index');
+Route::post('/register_user','UsersController@register');
+Route::post('/user_login','UsersController@login');
+Route::get('/logout','UsersController@logout');
+////// les valideurs ////////
+
+
+//////  Authentications ///////////
+Route::group(['middleware'=>'FrontLogin_middleware'],function (){
+    Route::get('/myaccount','UsersController@account');
+    Route::put('/update-profile/{id}','UsersController@updateprofile');
+    Route::put('/update-password/{id}','UsersController@updatepassword');
+    Route::get('/check-out','CheckOutController@index');
+    Route::post('/submit-checkout','CheckOutController@submitcheckout');
+    Route::get('/order-review','OrdersController@index');
+    Route::post('/submit-order','OrdersController@order');
+    Route::get('/cod','OrdersController@cod');
+    Route::get('/paypal','OrdersController@paypal');
+});
+///
+
+/* Admin  */
+Auth::routes(['register'=>false]);
+Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix'=>'admin','middleware'=>['auth','admin']],function (){
+    Route::get('/', 'AdminController@index')->name('admin_home');
+
+    /// Parametrage 
+    Route::get('/settings', 'AdminController@settings');
+    Route::get('/check-pwd','AdminController@chkPassword');
+    Route::post('/update-pwd','AdminController@updatAdminPwd');
+    /// Categories
+    Route::resource('/category','CategoryController');
+    Route::get('delete-category/{id}','CategoryController@destroy');
+    Route::get('/check_category_name','CategoryController@checkCateName');
+    /// Produits
+    Route::resource('/product','ProductsController');
+    Route::get('delete-product/{id}','ProductsController@destroy');
+    Route::get('delete-image/{id}','ProductsController@deleteImage');
+    ///  Attributs des produits
+    Route::resource('/product_attr','ProductAtrrController');
+    Route::get('delete-attribute/{id}','ProductAtrrController@deleteAttr');
+    ///  Images de la Galleie
+    Route::resource('/image-gallery','ImagesController');
+    Route::get('delete-imageGallery/{id}','ImagesController@destroy');
+    /// ///////// Coupons  //////////
+  //  Route::resource('/coupon','CouponController');
+  //  Route::get('delete-coupon/{id}','CouponController@destroy');
+///
+});
