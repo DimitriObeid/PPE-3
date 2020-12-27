8bd6da199b2b316f9f87ac9276eb4e4785f407c3
Mamad <barry30546@gmail.com>
2020-12-27 16:31:36 +0100

    Ajout d'application

---
+++ Application/resources/views/frontEnd/layouts/footer.blade.php
@@ -0,0 +1,153 @@
+ <footer id="footer">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-sm-7" style="margin-left: 244px;">
+                    <div class="col-sm-3">
+                        <div class="video-gallery text-center">
+                            <a href="#">
+                                <div class="iframe-img">
+                                    <img src="{{asset('frontEnd/images/home/iframe1.png')}}" alt="" />
+                                </div>
+                                <div class="overlay-icon">
+                                    <i class="fa fa-play-circle-o"></i>
+                                </div>
+                            </a>
+                            <p>Atelier</p>
+                            <h2>24 DEC 2020</h2>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-3">
+                        <div class="video-gallery text-center">
+                            <a href="#">
+                                <div class="iframe-img">
+                                    <img src="{{asset('frontEnd/images/home/iframe2.png')}}" alt="" />
+                                </div>
+                                <div class="overlay-icon">
+                                    <i class="fa fa-play-circle-o"></i>
+                                </div>
+                            </a>
+                            <p>Formations</p>
+                            <h2>24 DEC 20020</h2>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-3">
+                        <div class="video-gallery text-center">
+                            <a href="#">
+                                <div class="iframe-img">
+                                    <img src="{{asset('frontEnd/images/home/iframe3.png')}}" alt="" />
+                                </div>
+                                <div class="overlay-icon">
+                                    <i class="fa fa-play-circle-o"></i>
+                                </div>
+                            </a>
+                            <p>Création de société</p>
+                            <h2>24 DEC 2020</h2>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-3">
+                        <div class="video-gallery text-center">
+                            <a href="#">
+                                <div class="iframe-img">
+                                    <img src="{{asset('frontEnd/images/home/iframe4.png')}}" alt="" />
+                                </div>
+                                <div class="overlay-icon">
+                                    <i class="fa fa-play-circle-o"></i>
+                                </div>
+                            </a>
+                            <p>Conseils</p>
+                            <h2>24 DEC 2020</h2>
+                        </div>
+                    </div>
+                </div>
+            {{--    <div class="col-sm-3">
+                    <div class="address">
+                        <img src="{{asset('frontEnd/images/home/map.png')}}" alt="" />
+                        <p>10 Place de Gutemberg Cs 70 012,67081 Strasbourg</p>
+                    </div>
+                </div>  --}}
+            </div>
+        </div>
+    </div>
+
+  <!--  <div class="footer-widget">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-2">
+                    <div class="single-widget">
+                        <h2>Service</h2>
+                        <ul class="nav nav-pills nav-stacked">
+                            <li><a href="#">Online Help</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                            <li><a href="#">Order Status</a></li>
+                            <li><a href="#">Change Location</a></li>
+                            <li><a href="#">FAQ’s</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div class="single-widget">
+                        <h2>Quock Shop</h2>
+                        <ul class="nav nav-pills nav-stacked">
+                            <li><a href="#">T-Shirt</a></li>
+                            <li><a href="#">Mens</a></li>
+                            <li><a href="#">Womens</a></li>
+                            <li><a href="#">Gift Cards</a></li>
+                            <li><a href="#">Shoes</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div class="single-widget">
+                        <h2>Policies</h2>
+                        <ul class="nav nav-pills nav-stacked">
+                            <li><a href="#">Terms of Use</a></li>
+                            <li><a href="#">Privecy Policy</a></li>
+                            <li><a href="#">Refund Policy</a></li>
+                            <li><a href="#">Billing System</a></li>
+                            <li><a href="#">Ticket System</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div class="single-widget">
+                        <h2>About Shopper</h2>
+                        <ul class="nav nav-pills nav-stacked">
+                            <li><a href="#">Company Information</a></li>
+                            <li><a href="#">Carrière</a></li>
+                            <li><a href="#">Location</a></li>
+                            <li><a href="#">Affillate Program</a></li>
+                            <li><a href="#">Copyright</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-3 col-sm-offset-1">
+                    <div class="single-widget">
+                        <h2>About Shopper</h2>
+                        <form action="#" class="searchform">
+                            <input type="text" placeholder="Your email address" />
+                            <button type="submit" class="btn btn-default"><i class="fa fa-arrow-circle-o-right"></i></button>
+                            <p>Get the most recent updates from <br />our site and be updated your self...</p>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div> -->
+
+    <div class="footer-bottom" style="
+    background: darkblue;
+">
+        <div class="container">
+            <div class="row">
+                <p class="pull-left">Copyright © 2020 CCI-ALSACE. Tous droits reservés.</p>
+                <p class="pull-right">Conçu par <span><a target="_blank" href="#">CCI-ALSACE</a></span></p>
+            </div>
+        </div>
+    </div>
+</footer><!--/Footer-->
--- Application/routes/web.php
+++ Application/routes/web.php
@@ -1,14 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use App\Http\Controllers\CategorieController;
-use App\Http\Controllers\CommandesController;
-use App\Http\Controllers\EtatController;
-use App\Http\Controllers\FournituresController;
-use App\Http\Controllers\PersonnelController;
-use App\Http\Controllers\ServiceController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,9 +10,66 @@ use App\Http\Controllers\ServiceController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+/* FrontEnd  */
+Route::get('/','IndexController@index');
+Route::get('/list-products','IndexController@shop');
+Route::get('/cat/{id}','IndexController@listByCat')->name('cats');
+Route::get('/product-detail/{id}','IndexController@detialpro');
+////// Attribut ////////////
+Route::get('/get-product-attr','IndexController@getAttrs');
 
-Route::get('/', function () {
-    return view('connexion');
+///// Commandes /////////
+Route::post('/addToCart','CartController@addToCart')->name('addToCart');
+Route::get('/viewcart','CartController@index');
+Route::get('/cart/deleteItem/{id}','CartController@deleteItem');
+Route::get('/cart/update-quantity/{id}/{quantity}','CartController@updateQuantity');
+/////////////////////////
+
+/// Simple Utilisateur /////
+Route::get('/login_page','UsersController@index');
+Route::post('/register_user','UsersController@register');
+Route::post('/user_login','UsersController@login');
+Route::get('/logout','UsersController@logout');
+//////  valideur ////////
+
+
+//////  Authentications des utilisateurs ///////////
+Route::group(['middleware'=>'FrontLogin_middleware'],function (){
+    Route::get('/myaccount','UsersController@account');
+    Route::put('/update-profile/{id}','UsersController@updateprofile');
+    Route::put('/update-password/{id}','UsersController@updatepassword');
+    Route::get('/check-out','CheckOutController@index');
+    Route::post('/submit-checkout','CheckOutController@submitcheckout');
+    Route::get('/order-review','OrdersController@index');
+    Route::post('/submit-order','OrdersController@order');
+    Route::get('/cod','OrdersController@cod');
+    Route::get('/paypal','OrdersController@paypal');
 });
+///
 
-Route::get('/connexion', [PersonnelController::class, 'connexion']);
+/* Admin */
+Auth::routes(['register'=>false]);
+Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix'=>'admin','middleware'=>['auth','admin']],function (){
+    Route::get('/', 'AdminController@index')->name('admin_home');
+
+    /// Setting
+    Route::get('/settings', 'AdminController@settings');
+    Route::get('/check-pwd','AdminController@chkPassword');
+    Route::post('/update-pwd','AdminController@updatAdminPwd');
+    /// Categorie
+    Route::resource('/category','CategoryController');
+    Route::get('delete-category/{id}','CategoryController@destroy');
+    Route::get('/check_category_name','CategoryController@checkCateName');
+    /// Produits
+    Route::resource('/product','ProductsController');
+    Route::get('delete-product/{id}','ProductsController@destroy');
+    Route::get('delete-image/{id}','ProductsController@deleteImage');
+    /// Produit / Attribut
+    Route::resource('/product_attr','ProductAtrrController');
+    Route::get('delete-attribute/{id}','ProductAtrrController@deleteAttr');
+    /// Images Galerie
+    Route::resource('/image-gallery','ImagesController');
+    Route::get('delete-imageGallery/{id}','ImagesController@destroy');
+ 
+});
